2f017a19c54d30116299fb73f9f129b1ad1931bb
Victor <victorplanchuelo@gmail.com>
2020-02-06 17:24:59 +0100

    Created products and categories relationship

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Product;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +14,27 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    /*
+    $prod = new Product();
+    $prod->nombre = 'Producto 3';
+    $prod->slug = 'producto-3';
+    $prod->category_id = 2;
+    $prod->descripcion_corta = 'Descripción corta del producto 3';
+    $prod->descripcion_larga = 'Esta es la descripción más larga del producto 3';
+    $prod->especificaciones = 'Especificaciones del producto 3';
+    $prod->datos_de_interes = 'Los datos de interés del producto 3';
+    $prod->estado = 'Nuevo';
+    $prod->activo = 'Si';
+    $prod->sliderprincipal = 'No';
+
+    $prod->save();
+    return $prod;
+
+    */
+$prod = Product::find(1)->category;
+return $prod;
+
+    //return view('welcome');
 });
 
 Auth::routes();
